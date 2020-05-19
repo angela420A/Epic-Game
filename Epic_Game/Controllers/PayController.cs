bdce9f1967756cd1fac5c5ff714f87aca2c8f2cf
a0975283413@gmail.com <a0975283413@gmail.com>
2020-05-19 17:07:32 +0800

    增加付款畫面與合併

---
+++ Epic_Game/Controllers/PayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Epic_Game.Controllers
+{
+    public class PayController : Controller
+    {
+        // GET: Pay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
