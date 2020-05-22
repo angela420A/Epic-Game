ea2c0e6e822c8ae970c85f23121a860ef768df31
Eileen <a0908802569@gmail.com>
2020-05-22 17:40:45 +0800

    產品頁面更改

--- Epic_Game/Assets/Js/product_js.js
+++ Epic_Game/Assets/Js/product_js.js
@@ -1,3 +1,4 @@
+//search
 var searchbar = document.querySelector('.navbar .form-control');
 var searchnav = document.querySelector('svg');
 var onoff = false;
@@ -13,3 +14,44 @@ searchnav.addEventListener('click', function () {
         onoff = false;
     }
 });
+// star rating
+var $star_rating = $('.star-rating .fa');
+
+var SetRatingStar = function () {
+    return $star_rating.each(function () {
+        if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+            return $(this).removeClass('fa-star-o').addClass('fa-star');
+        } else {
+            return $(this).removeClass('fa-star').addClass('fa-star-o');
+        }
+    });
+};
+
+$star_rating.on('click', function () {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+});
+
+SetRatingStar();
+$(document).ready(function () {
+
+});
+//SHOW MORE
+function SHOWMORE() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "SHOW MORE";
+        moreText.style.display = "none";
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "SHOW LESS";
+        moreText.style.display = "inline";
+
+
+    }
+}
