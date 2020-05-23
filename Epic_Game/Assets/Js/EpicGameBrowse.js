6f3de475641d8a6edae5343deb64b2ade02520b0
俊佑 郭 <geneissmart@gmail.com>
2020-05-23 23:43:49 +0800

    修改收尋

---
+++ Epic_Game/Assets/Js/EpicGameBrowse.js
@@ -0,0 +1,20 @@
+var searchbar = document.querySelector('.navbar .form-control');
+var searchnav = document.querySelector('svg');
+var onoff = false;
+var li = document.querySelector('.div_itemlist li');
+console.log(searchbar);
+searchnav.addEventListener('click', function () {
+    if (onoff == false) {
+        searchbar.setAttribute('style', 'display: block');
+        onoff = true;
+    }
+    else if (onoff == true) {
+        searchbar.setAttribute('style', 'display: none');
+        onoff = false;
+    }
+});
+    
+    
+  
+  
+
