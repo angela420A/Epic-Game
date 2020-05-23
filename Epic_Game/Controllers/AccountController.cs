f591784da9e54a26bed55690f1c399f47848f62c
mizugakun@gmail.com <mizugakun@gmail.com>
2020-05-23 20:55:42 +0800

    修正 無法以E-mail登入已有帳號的問題

--- Epic_Game/Controllers/AccountController.cs
+++ Epic_Game/Controllers/AccountController.cs
@@ -75,7 +75,8 @@ namespace Epic_Game.Controllers
 
             // 這不會計算為帳戶鎖定的登入失敗
             // 若要啟用密碼失敗來觸發帳戶鎖定，請變更為 shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var user = await UserManager.FindByEmailAsync(model.Email);
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
