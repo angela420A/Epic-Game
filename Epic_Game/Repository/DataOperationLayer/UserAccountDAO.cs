c5415c4f9cc3afe03a63decda08e3e817cd6c133
mizugakun@gmail.com <mizugakun@gmail.com>
2020-05-24 08:47:05 +0800

    新增 UserAccountBLO 新增 UserAccountDAO 新增 UserAccountViewModel 修改 進度帳號管理介面URL 修改 進入帳號管理介面時讀取使用者資料

---
+++ Epic_Game/Repository/DataOperationLayer/UserAccountDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EpicGameLibrary.Models;
+using EpicGameLibrary.Repository;
+using Microsoft.AspNet.Identity;
+
+
+namespace Epic_Game.Repository.DataOperationLayer
+{
+    public class UserAccountDAO
+    {
+        EGRepository<AspNetUsers> repo;
+        EGContext context;
+
+        public UserAccountDAO()
+        {
+            context = new EGContext();
+            repo = new EGRepository<AspNetUsers>(context);
+        }
+
+        public AspNetUsers getUserById(string UserId)
+        {
+            return context.AspNetUsers.Single(x => x.Id == UserId);
+        }
+    }
+}
