abd596dff373b611f5136c603de1a6beaad538ca
Afan <kpopganggg@gmail.com>
2020-05-29 12:32:55 +0800

    新聞內容假資料

---
+++ Epic_Game/Assets/Js/Articlejs.js
@@ -0,0 +1,5 @@
+﻿var iframe = document.getElementsByTagName("iframe");
+for (let i = 0; i < iframe.length; i++) {
+    var div = document.getElementsByTagName("iframe")[i].parentNode;
+    div.className = "iframeclass";
+}
