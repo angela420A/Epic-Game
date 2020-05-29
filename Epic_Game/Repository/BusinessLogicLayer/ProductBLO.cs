153e3e19a2c63b7e70191954c6843f80aaa9a1ea
Eileen <a0908802569@gmail.com>
2020-05-29 17:49:38 +0800

    建ProductViewModels

---
+++ Epic_Game/Repository/BusinessLogicLayer/ProductBLO.cs
@@ -0,0 +1,19 @@
+﻿using Epic_Game.Repository.DataOperationLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game.Repository.BusinessLogicLayer
+{
+    public class ProductBLO
+    {
+        private ProductDAO ProductDAO { get; set; }
+        public ProductBLO(string ProductId)
+        {
+            //ProductDAO = new ProductDAO(ProductId);
+        }
+
+
+    }
+}
---
+++ Epic_Game/ViewModels/ProductViewModel.cs
@@ -0,0 +1,36 @@
+﻿using EpicGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Image_URL { get; set; }
+        public Guid PD_ProductID { get; set; }
+        public string PD_ProductName { get; set; }
+        public string PD_ContentType { get; set; }
+        public string PD_Title { get; set; }
+        public decimal PD_Price { get; set; }
+        public decimal PD_Discount { get; set; }
+        public string PD_Publisher { get; set; }
+        public string PD_ReleaseDate { get; set; }
+        public string PD_Category { get; set; }
+        public string PD_AgeRestriction { get; set; }
+        public int OS { get; set; }
+        public string PD_Description { get; set; }
+        public string SM_URL { get; set; }
+        public string SM_Community { get; set; }
+        public Image Pack_image { get; set; }
+        public decimal Pack_Price { get; set; }
+        public decimal Pack_Discount { get; set; }
+        public string Library_Condition { get; set; }
+        public string Comment_Title { get; set; }
+        public DateTime Comment_Date { get; set; }
+        public string Comment_Description { get; set; }
+        public int Comment_Rank { get; set; }
+
+    }
+}
