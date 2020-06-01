29b96cc16e2599f294a8134cd0ceefa7f918d3f3
Constant Wu <cc26817718@gmail.com>
2020-06-01 17:09:42 +0800

    新增 Library分層

---
+++ Epic_Game/Repository/BusinessLogicLayer/LibraryBLO.cs
@@ -0,0 +1,38 @@
+﻿using Epic_Game.Repository.DataOperationLayer;
+using Epic_Game.ViewModels;
+using EpicGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using WebGrease.Css.Extensions;
+
+namespace Epic_Game.Repository.BusinessLogicLayer
+{
+    public class LibraryBLO
+    {
+        private LibraryDAO libraryDAO { get; set; }
+
+        public LibraryBLO(string UserId)
+        {
+            libraryDAO = new LibraryDAO(UserId);
+        }
+        public LibraryViewModel LibraryToView(Library l,Image i)
+        {
+            var viewModel = new LibraryViewModel
+            {
+                ProductName = l.Product.ProductName,
+                Img_Url = i.Url
+            };
+            return viewModel;
+        }
+        public LibraryViewModel GetLibraryProduct()
+        {
+            var l = libraryDAO.GetLibraryProduct();
+            var imgUrl = libraryDAO.GetImg();
+            return LibraryToView(l, imgUrl);
+        }
+
+    }
+}
---
+++ Epic_Game/Repository/DataOperationLayer/LibraryDAO.cs
@@ -0,0 +1,29 @@
+﻿using EpicGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EpicGameLibrary.Repository;
+
+namespace Epic_Game.Repository.DataOperationLayer
+{
+    public class LibraryDAO
+    {
+        public Image img;
+        public EGContext context;
+        public Product product;
+        public Library library;
+        public LibraryDAO(string UserId)
+        {
+            library = context.Library.Single(x => x.UserID == UserId);
+        }
+        public Library GetLibraryProduct()
+        {
+            return library;
+        }
+        public Image GetImg()
+        {
+            return img;
+        }
+    }
+}
