9cea1a02634f5e9ad9c26910f73d1676b490c54d
mizugakun@gmail.com <mizugakun@gmail.com>
2020-06-04 16:23:56 +0800

    修改 Specificatin 表格

--- EpicGameLibrary/Models/Specifications.cs
+++ EpicGameLibrary/Models/Specifications.cs
@@ -9,23 +9,48 @@ namespace EpicGameLibrary.Models
     public partial class Specifications
     {
         [Key]
+        [Column(Order = 0)]
         public Guid ProductID { get; set; }
 
-        [StringLength(100)]
+        [StringLength(200)]
         public string OS { get; set; }
 
-        [StringLength(100)]
-        public string Processor { get; set; }
-
-        [StringLength(50)]
-        public string RAM { get; set; }
+        [StringLength(200)]
+        public string CPU { get; set; }
 
         [StringLength(200)]
-        public string GraphicsCard { get; set; }
+        public string GPU { get; set; }
+
+        [StringLength(200)]
+        public string Processor { get; set; }
+
+        [StringLength(200)]
+        public string RAM { get; set; }
+
+        [StringLength(50)]
+        public string Memory { get; set; }
+
+        [StringLength(50)]
+        public string Storage { get; set; }
+
+        [StringLength(200)]
+        public string GraphiceCard { get; set; }
 
         [StringLength(50)]
         public string HDD { get; set; }
 
-        public int? Typ { get; set; }
+        [StringLength(50)]
+        public string DirectX { get; set; }
+
+        [Column("Additional Features")]
+        [StringLength(200)]
+        public string Additional_Features { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Type { get; set; }
+
+        public virtual Product Product { get; set; }
     }
 }
