b3816e1d0af90001a46ea312011f23a90ade3652
mizugakun@gmail.com <mizugakun@gmail.com>
2020-06-05 02:30:37 +0800

    新增 GameType Service 新增 後台Controller 新增 商品管理Controller View

---
+++ Epic_Game/Controllers/Backstage/ProductManageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EpicGameLibrary.Models;
+
+namespace Epic_Game.Controllers.Backstage
+{
+    public class ProductManageController : Controller
+    {
+        // GET: ProductManage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductName, Price, Discount")] Product p)
+        {
+            return View();
+        }
+    }
+}
---
+++ Epic_Game/Service/GameType.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game.Service
+{
+    public class GameType
+    {
+        [Flags]
+        enum gametype
+        {
+            Action = 1,
+            Adventure = 2,
+            Editors = 4,
+            Puzzle = 8,
+            Racing = 16,
+            RPG = 32,
+            Shooter = 64,
+            Strategy = 128,
+            Survival = 256,
+            ControllerSupport = 512,
+            CoOp = 1024,
+            SinglePlayer = 2048,
+            Multiplayer = 4096,
+            Windows = 8192,
+            MacOS = 16384,
+        }
+
+        public List<string> getGameType(int n)
+        {
+            List<string> res = new List<string>();
+            string[] str = Enum.GetNames(typeof(gametype));
+            int i = 0;
+            while (n != 0)
+            {
+                if ((n & (~n + 1)) == 1) res.Add(str[i]);
+                i++;
+                n = n >> 1;
+            }
+            return res;
+        }
+
+        public bool searchGameType(int n, int type)
+        {
+            int i = 1;
+            while (n != 0)
+            {
+                if ((n & (~n + 1)) == 1 && i == type) return true;
+                i *= 2;
+                n = n >> 1;
+            }
+            return false;
+        }
+    }
+}
