f4b6cc459160fe76d6751b9818a6eda6b1121fb1
mizugakun@gmail.com <mizugakun@gmail.com>
2020-06-04 14:41:19 +0800

    修改 Activity IMG為必要欄位 新增 Image Imguid 欄位 新增 Image Type 欄位 新增 Pack PackName欄位 新增 Pack Title欄位 新增 Pack Description欄位 新增 Pack ReleaseDate欄位 新增 Product LanguagesSupported欄位 新增 Product PrivatePolicy欄位 新增 Product PrivatePolicyUrl欄位 新增 Specifycations表格

--- EpicGameLibrary/Models/Activity.cs
+++ EpicGameLibrary/Models/Activity.cs
@@ -24,6 +24,7 @@ namespace EpicGameLibrary.Models
         [StringLength(100)]
         public string Information { get; set; }
 
+        [Required]
         public string IMG { get; set; }
     }
 }
