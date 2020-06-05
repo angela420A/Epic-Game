be323228b5e18d6070494b87cf31fc36df95a9a5
a0975283413@gmail.com <a0975283413@gmail.com>
2020-06-05 16:09:18 +0800

    ECPay

--- Epic_Game/Controllers/PayController.cs
+++ Epic_Game/Controllers/PayController.cs
@@ -1,10 +1,12 @@
-﻿using Epic_Game.Repository.BusinessLogicLayer;
+﻿using ECPay.Payment.Integration;
+using Epic_Game.Repository.BusinessLogicLayer;
 using Epic_Game.ViewModels;
 using EpicGameLibrary.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,7 +14,12 @@ namespace Epic_Game.Controllers
 {
     public class PayController : Controller
     {
-        
+        private PayBLO PayBLO { get; set; }
+
+        public PayController()
+        {
+            PayBLO = new PayBLO();
+        }
 
         // GET: Pay
         public ActionResult Index()
@@ -20,9 +27,8 @@ namespace Epic_Game.Controllers
             //string ProductID = (string)TempData["ProductId"];
             string ProductID = "d75ebeb8-4bc7-44b3-86bf-904ec05a5686";
             var UserId = User.Identity.GetUserId();
-            PayBLO payBLO = new PayBLO();
-            PayViewModel VM = payBLO.GetPayViewModel(ProductID,UserId);
-            var hasgame = payBLO.GetLibrary(UserId,ProductID);
+            PayViewModel VM = PayBLO.GetPayViewModel(ProductID);
+            var hasgame = PayBLO.GetLibrary(UserId,ProductID);
             if (UserId == null)
             {
                 return RedirectToAction("Login", "Account");
@@ -43,6 +49,23 @@ namespace Epic_Game.Controllers
         public ActionResult Finish()
         {
             return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Finish([Bind(Include = "ProductId,ProductName")] PayViewModel info)
+        //{
+        //    return View();
+        //}
+
+
+        public string ToECpay(string jdata)
+        {
+            PayViewModel VM = PayBLO.GetPayViewModel(jdata);
+            Session["Name"] = VM.ProductName;
+            Session["P"] = string.Format("{0:####.#}", Session["Price"]= VM.Price);
+
+            return "../EpicGameCheckOut.aspx";
         }
     }
 }
