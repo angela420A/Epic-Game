3173f53bdca82f65b8092976262579278bb09941
俊佑 郭 <geneissmart@gmail.com>
2020-06-07 00:34:59 +0800

    刪除註解

--- Epic_Game/Repository/BusinessLogicLayer/HomeBLO.cs
+++ Epic_Game/Repository/BusinessLogicLayer/HomeBLO.cs
@@ -17,20 +17,9 @@ namespace Epic_Game.Repository.BusinessLogicLayer
             HomeDAO = new HomeDAO();
         }
 
-        //public HomeViewModels GetHomeViewModel()
-        //{
-        //    var data = HomeDAO.GetProducts();
-        //    return new HomeViewModels
-        //    {
-        //        BestDiscount = HomeDAO.GetProducts().OrderBy(x => x.Discount).Take(5).ToList(),
-        //        Activities = GetHomeActivity().ToList(),
-        //    };
-        //}
 
         public HomeViewModels getHomeViewModel()
         {
-            //var sales = HomeDAO.GetSales().GroupBy(x => x.ProductID).Select(s => new GroupList() { Key = s.Key, Count = s.Count() }).OrderByDescending(o => o.Count).ToList();
-            //var topSales = HomeDAO.getTop5Sale(sales);
             HomeViewModels homeViewModels = new HomeViewModels();
 
             homeViewModels.TopSales = HomeDAO.getTopSales(); ;
@@ -40,16 +29,6 @@ namespace Epic_Game.Repository.BusinessLogicLayer
             homeViewModels.BestRank = HomeDAO.getTopBestRank();
             return homeViewModels;
         }
-
-
-        //public IEnumerable<HomeActivityViewModels> GetHomeActivity()
-        //{
-        //    var ActivityData = HomeDAO.GetActivity();
-        //    return ActivityData;
-        //}
-
-        //public IEnumerable<>
-
 
     }
 }
--- Epic_Game/Repository/DataOperationLayer/HomeDAO.cs
+++ Epic_Game/Repository/DataOperationLayer/HomeDAO.cs
@@ -81,22 +81,22 @@ namespace Epic_Game.Repository.DataOperationLayer
             return MostRelated;
         }
 
-        public List<StoreItems> getTopMostPopular()
-        {
-            List<StoreItems> MostPopular;
-            using (conn = new SqlConnection(connString))
-            {
-                string sql = @"select TOP 5
-                            sum(c.Rank) as rank,img.Url,p.ProductName,p.Developer,p.Publisher,p.Discount,p.Price
-                            from Product p
-                            inner join Image img on p.ProductID = img.ProductOrPack
-                            inner join Comment c on p.ProductID = c.ProductID
-                            group by c.ProductID,img.Url,p.ProductName,p.Developer,p.Publisher,p.Discount,p.Price
-                            order by rank desc";
-                MostPopular = conn.Query<StoreItems>(sql).ToList();
-            }
-            return MostPopular;
-        }
+        //public List<StoreItems> getTopMostPopular()
+        //{
+        //    List<StoreItems> MostPopular;
+        //    using (conn = new SqlConnection(connString))
+        //    {
+        //        string sql = @"select TOP 5
+        //                    sum(c.Rank) as rank,img.Url,p.ProductName,p.Developer,p.Publisher,p.Discount,p.Price
+        //                    from Product p
+        //                    inner join Image img on p.ProductID = img.ProductOrPack
+        //                    inner join Comment c on p.ProductID = c.ProductID
+        //                    group by c.ProductID,img.Url,p.ProductName,p.Developer,p.Publisher,p.Discount,p.Price
+        //                    order by rank desc";
+        //        MostPopular = conn.Query<StoreItems>(sql).ToList();
+        //    }
+        //    return MostPopular;
+        //}
 
 
         public List<StoreItems> getTopBestRank()
@@ -116,34 +116,5 @@ namespace Epic_Game.Repository.DataOperationLayer
             return BestRank;
         }
 
-
-
-        //public List<StoreItems> GetSales()
-        //{
-        //    var Sales = (from o in context.Order
-        //                 join p in context.Product on o.ProductID equals p.ProductID
-        //                 join i in context.Image on p.ProductID equals i.ProductOrPack
-        //                 where i.Location == 0
-        //                 select new StoreItems() { ProductID = o.ProductID, Url = i.Url, ProductName = p.ProductName, Developer = p.Developer, Publisher = p.Publisher, Discount = p.Discount, Price = p.Price }).ToList();
-
-        //    return Sales;
-        //}
-
-        //public List<StoreItems> getTop5Sale(List<GroupList> groupLists)
-        //{
-        //    List<StoreItems> storeItems = new List<StoreItems>();
-
-        //    for (int i = 0; i < (groupLists.Count >= 5 ? 5 : groupLists.Count); i++)
-        //    {
-        //        var s = groupLists[i].Key;
-        //        var storeItem =  from p in context.Product
-        //                         join img in context.Image on p.ProductID equals img.ProductOrPack
-        //                         where p.ProductID == groupLists[i].Key && img.Location == 0
-        //                         select new StoreItems() { ProductID = p.ProductID, Url = img.Url, ProductName = p.ProductName, Developer = p.Developer, Publisher = p.Publisher, Discount = p.Discount, Price = p.Price };
-        //        storeItems.Add(storeItem.FirstOrDefault());
-        //    }
-
-        //    return storeItems;
-        //}
     }
 }
