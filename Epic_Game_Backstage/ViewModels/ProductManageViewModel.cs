ba763fcfb72dc86a004b152a7ec8b0a00a525c7a
mizugakun@gmail.com <mizugakun@gmail.com>
2020-06-07 09:05:09 +0800

    新增 後台用專案Epic_Game_Backstage

---
+++ Epic_Game_Backstage/ViewModels/ProductManageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Epic_Game_Backstage.ViewModels
+{
+    public class ProductManageViewModel
+    {
+        public string ProductID { get; set; }
+
+        public string ProductImage { get; set; }
+         
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ContentType { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Developer { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Publisher { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public List<string> Categories { get; set; }
+
+    }
+}
