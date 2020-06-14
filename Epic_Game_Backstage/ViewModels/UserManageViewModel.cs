fac249e71168006d49267b2eb4d3e4a7c36a78a3
angela <lv0919327584@gmail.com>
2020-06-14 22:06:21 +0800

    新增 後台抓取使用者資料介面及DAO BLO

---
+++ Epic_Game_Backstage/ViewModels/UserManageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game_Backstage.ViewModels
+{
+    public class UserManageViewModel
+    {
+        public string UserName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public int? PostalCode { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
