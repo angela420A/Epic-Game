90bee0d01435994d3cf0e45147a04a5f2de54383
家寶 吳 <cc26817718@gmail.com>
2020-06-17 18:10:55 +0800

    新增 商品Detail

---
+++ Epic_Game_Backstage/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game_Backstage.ViewModels
+{
+    public class ProductDetailsViewModel
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Img_Url { get; set; }
+        public string Developer { get; set; }
+        public string Publisher { get; set; }
+        public string ReleaseDate { get; set; }
+        public int sales_volume { get; set; }
+        public int total_income { get; set; } //新台幣
+    }
+}
