f31b409cd5e0772f892503ed775f66bc35107b60
angela <lv0919327584@gmail.com>
2020-06-17 17:21:16 +0800

    修改 後台Layout和Dashbord

---
+++ Epic_Game_Backstage/Repository/DataAccessLayer/BackstageHomeDAO.cs
@@ -0,0 +1,21 @@
+﻿using Epic_Game_Backstage.ViewModels;
+using EpicGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epic_Game_Backstage.Repository.DataAccessLayer
+{
+    public class BackstageHomeDAO
+    {
+        private EGContext context;
+
+        public BackstageHomeDAO()
+        {
+            context = new EGContext();
+        }
+
+        
+    }
+}
