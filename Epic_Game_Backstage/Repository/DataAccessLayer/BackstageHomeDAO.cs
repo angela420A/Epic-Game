adb3d9b92a2c5906bac46efd21354eccb9866efa
angela <lv0919327584@gmail.com>
2020-06-19 00:37:37 +0800

    新增 後台DasehBord  - Chart Line抓資料庫資料

--- Epic_Game_Backstage/Repository/DataAccessLayer/BackstageHomeDAO.cs
+++ Epic_Game_Backstage/Repository/DataAccessLayer/BackstageHomeDAO.cs
@@ -25,9 +25,9 @@ namespace Epic_Game_Backstage.Repository.DataAccessLayer
             }
         }
 
-        public List<BackstageHomeViewModel> getSingledata()
+        public List<BackstageSingleDataVM> getSingledata()
         {
-            List<BackstageHomeViewModel> backstageHomeVM;
+            List<BackstageSingleDataVM> backstageHomeVM;
             using (conn = new SqlConnection(connString))
             {
                 string sql = @"SELECT 
@@ -41,7 +41,7 @@ namespace Epic_Game_Backstage.Repository.DataAccessLayer
                                 inner join AspNetUsers u on l.UserID = u.Id
                                ";
 
-                backstageHomeVM = conn.Query<BackstageHomeViewModel>(sql).ToList();
+                backstageHomeVM = conn.Query<BackstageSingleDataVM>(sql).ToList();
 
                 //string totalPrice = @"select SUM(Payment) from [Order]";
                 //string orderQuantity = @"select COUNT(*) from [Order]";
@@ -58,6 +58,54 @@ namespace Epic_Game_Backstage.Repository.DataAccessLayer
                 //    }
                 //};
 
+            }
+            return backstageHomeVM;
+        }
+
+        public List<BackstageChartLineVM> getMonthData()
+        {
+            List<BackstageChartLineVM> backstageHomeVM;
+
+            using (conn = new SqlConnection(connString))
+                {
+                    string sql = @"SELECT 
+	                            SUM(o.Payment) AS January,
+	                            (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'February') AS February,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'March') AS March,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'April') AS April,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'May') AS May,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'June') AS June,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'July') AS July,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'August') AS August,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'September') AS September,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'October') AS October,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'November') AS November,
+	                             (select SUM(o.Payment)
+	                             from [Order] o
+	                             WHERE DATENAME(month, o.Date) = 'December') AS December
+                            from [Order] o
+                            WHERE DATENAME(month, o.Date) = 'January'";
+                backstageHomeVM = conn.Query<BackstageChartLineVM>(sql).ToList();
             }
             return backstageHomeVM;
         }
--- Epic_Game_Backstage/ViewModels/BackstageHomeViewModel.cs
+++ Epic_Game_Backstage/ViewModels/BackstageHomeViewModel.cs
@@ -7,9 +7,32 @@ namespace Epic_Game_Backstage.ViewModels
 {
     public class BackstageHomeViewModel
     {
+        public List<BackstageSingleDataVM> backstageSingleDataVM;
+        public List<BackstageChartLineVM> backstageChartLineVM;
+        public int[] monthData { get; set; }
+    }
+    public class BackstageSingleDataVM
+    {
         public string ProductQuantity { get; set; }
         public string TotalPrice { get; set;}
         public string OrderQuantity { get; set; }
         public string UserQuantity { get; set; }
+
+    }
+
+    public class BackstageChartLineVM
+    {
+        public int January { get; set; }
+        public int February { get; set; }
+        public int March { get; set; }
+        public int April { get; set; }
+        public int May { get; set; }
+        public int June { get; set; }
+        public int July { get; set; }
+        public int August { get; set; }
+        public int September { get; set; }
+        public int October { get; set; }
+        public int November { get; set; }
+        public int December { get; set; }
     }
 }
