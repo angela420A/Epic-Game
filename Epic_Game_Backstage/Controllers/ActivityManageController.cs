3f237a1c5a22d9bcc83ce5ff4ee18cbf663828b6
Eileen <a0908802569@gmail.com>
2020-06-19 11:10:59 +0800

    新增 創建的 BLO DAO Controller

---
+++ Epic_Game_Backstage/Controllers/ActivityManageController.cs
@@ -0,0 +1,70 @@
+﻿using Epic_Game_Backstage.Repository.BusinessLogicLayer;
+using Epic_Game_Backstage.ViewModels;
+using EpicGameLibrary.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Epic_Game_Backstage.Controllers
+{
+    public class ActivityManageController : Controller
+    {
+        private ActivityManageBLO blo;
+        // GET: Activity
+        public ActivityManageController()
+        {
+            blo = new ActivityManageBLO();
+        }
+        public ActionResult Index()
+        {
+            var vm = blo.GetActivityManageView();
+            return View(vm);
+        }
+        // GET: ProductManage/Create
+        public ActionResult Create()
+        {
+            ActivityViewModel data = new ActivityViewModel();
+            return View(data);
+            //return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string jdata)
+        {
+            if (jdata == null) return HttpNotFound("Error");
+            ActivityViewModel AVM = JsonConvert.DeserializeObject<ActivityViewModel>(jdata);
+            blo = new ActivityManageBLO();
+            blo.ViewToModel(AVM);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+
+        public ActionResult Delete()
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public void UploadImg(string id)
+        {
+
+        }
+    }
+}
---
+++ Epic_Game_Backstage/Repository/BusinessLogicLayer/ActivityManageBLO.cs
@@ -0,0 +1,59 @@
+﻿using Epic_Game_Backstage.Repository.DataAccessLayer;
+using Epic_Game_Backstage.ViewModels;
+using EpicGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Epic_Game_Backstage.Repository.BusinessLogicLayer
+{
+    public class ActivityManageBLO
+    {
+        private ActivityManageDAO dao;
+        public List<ActivityViewModel> GetActivityManageView()
+        {
+            dao = new ActivityManageDAO();
+            var Activity = dao.GetAllActivities();
+            return ModelToView(Activity);
+        }
+
+        private List<ActivityViewModel> ModelToView(List<Activity> a)
+        {
+            dao = new ActivityManageDAO();
+            var result = new List<ActivityViewModel>();
+            foreach(var i in a)
+            {
+                try
+                {
+                    var item = new ActivityViewModel
+                    {
+                        Picture = i.IMG,
+                        Title = i.Slogan,
+                        ProductName = i.ActivityName,
+                        Content = i.Information,
+                    };
+                    result.Add(item);
+                }
+                catch (Exception ex) { }
+                finally { }
+            }
+            return result;
+        }
+        public void ViewToModel(ActivityViewModel AVM)
+        {
+            dao = new ActivityManageDAO();
+            var a = new Activity()
+            {
+                ActivityName = AVM.ProductName,
+                Slogan = AVM.Title,
+                Information = AVM.Content,
+                IMG = AVM.Picture
+            };
+            dao.CreateActivity(a);
+
+        }
+
+    }
+}
